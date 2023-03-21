50f67dd9dcb6c69d9e1635cad60049c4a2b75e47
Hanif Rodili <hanifrodili@gmail.com>
2023-03-21 15:14:20 +0800

    Update index to coming soon

--- app.vue
+++ app.vue
@@ -1,8 +1,10 @@
 <template lang="pug">
 .app
-  MainNav
-  NuxtPage.main-content
-  NuxtSnackbar
+  //- MainNav
+  //- NuxtPage.main-content
+  //- NuxtSnackbar
+  nuxt-layout
+    nuxt-page
 </template>
 
 <script setup>
--- components/general/ComingSoon.vue
+++ components/general/ComingSoon.vue
@@ -1,32 +1,16 @@
 <template lang="pug">
-v-container
-  v-row(style='position: relative; top: 10vh')
-    v-col(cols='12', align='center')
-      v-card.my-2(
-        min-height='200',
-        max-width='500',
-        flat,
-        tile,
-        color='transparent'
-      )
-        v-img(:src='img', max-width='200',)
-        .text-h4.text-secondary.my-4
-          b 
-            b Coming Soon
+v-container.d-flex.flex-column.align-center.justify-center(style="height:100vh")
+  v-card.my-2(
+    min-height='200',
+    max-width='500',
+    flat,
+    tile,
+    color='transparent'
+  )
+    v-img(:src="'/icon.svg'", max-width='200',)
+    h1.text-h4.text-secondary.my-4.font-weight-bold Coming Soon
 </template>
 
-<script>
-export default {
-  data() {
-    return {};
-  },
-  computed: {
-    img() {
-      return `${useRuntimeConfig().baseUrl}icon.png`;
-    },
-  },
-  methods: {},
-};
-</script>
+<script></script>
 
 <style scoped></style>
