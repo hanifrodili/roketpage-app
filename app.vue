68155b27ec5ddbf198ed2bd94d4e544fc17c9fcc
Hanif Rodili <hanifrodili@gmail.com>
2023-03-21 14:39:41 +0800

    Update setup

--- app.vue
+++ app.vue
@@ -2,31 +2,12 @@
 .app
   MainNav
   NuxtPage.main-content
+  NuxtSnackbar
 </template>
 
 <script setup>
 </script>
 
 <style lang="scss">
-@font-face {
-  font-family: 'Montserrat';
-  src: url('/fonts/Montserrat-VariableFont_wght.ttf');
-}
-@font-face {
-  font-family: 'Open Sans';
-  src: url('/fonts/OpenSans-VariableFont_wdth,wght.ttf');
-}
-
-body {
-  font-family: 'Open Sans';
-}
-
-h1, h2, h3, h4, h5, h6 {
-  font-family: 'Montserrat';
-}
-
-.main-content {
-  padding: 30px 40px;
-  padding-top: 100px;
-}
+@use "./public/variables";
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,86 +2,104 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-      title: 'RoketPage',
-      titleTemplate: '%s | Help your sales to the moon 🚀',
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "RoketPage",
+      titleTemplate: "%s | Help your sales to the moon 🚀",
       link: [
-        { rel: 'icon', type: 'image/png', href: '/favicon.svg' }
+        { rel: 'icon', type: 'image/svg', href: '/icon.svg' }
       ],
       meta: [
-      { charset: 'utf-8' },
-      {
-        name: 'viewport',
-        content:
-          'width=device-width, initial-scale=1 maximum-scale=1, user-scalable=no',
-      },
-      {
-        hid: 'description',
-        name: 'description',
-        content: 'To help no tech customer setting up a new sales funnel',
-      },
-      {
-        hid: 'keywords',
-        name: 'keywords',
-        content: 'ecommmerce, sales page, sales,',
-      },
-      // og metas
-      {
-        hid: 'og:title',
-        property: 'og:title',
-        content: 'roketpage.com',
-      },
-      {
-        hid: 'og:description',
-        property: 'og:description',
-        content: 'Help your sales to the moon 🚀',
-      },
-      {
-        hid: 'og:image',
-        property: 'og:image',
-        content: `${process.env.BASE_URL}icon.png`,
-      },
-      {
-        hid: 'og:url',
-        property: 'og:url',
-        content: `${process.env.BASE_URL}`,
-      },
+        { charset: "utf-8" },
+        {
+          name: "viewport",
+          content:
+            "width=device-width, initial-scale=1 maximum-scale=1, user-scalable=no",
+        },
+        {
+          hid: "description",
+          name: "description",
+          content: "To help no tech customer setting up a new sales funnel",
+        },
+        {
+          hid: "keywords",
+          name: "keywords",
+          content: "ecommmerce, sales page, sales,",
+        },
+        // og metas
+        {
+          hid: "og:title",
+          property: "og:title",
+          content: "roketpage.com",
+        },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content: "Help your sales to the moon 🚀",
+        },
+        {
+          hid: "og:image",
+          property: "og:image",
+          content: `${process.env.BASE_URL}icon.png`,
+        },
+        {
+          hid: "og:url",
+          property: "og:url",
+          content: `${process.env.BASE_URL}`,
+        },
 
-      // twitter card
-      {
-        hid: 'twitter:card',
-        name: 'twitter:card',
-        content: 'summary',
-      },
-      {
-        hid: 'twitter:creator',
-        name: 'twitter:creator',
-        content: '',
-      },
+        // twitter card
+        {
+          hid: "twitter:card",
+          name: "twitter:card",
+          content: "summary",
+        },
+        {
+          hid: "twitter:creator",
+          name: "twitter:creator",
+          content: "",
+        },
 
-      {
-        hid: 'twitter:site',
-        name: 'twitter:site',
-        content: '',
-      },
-    ],
-    }
+        {
+          hid: "twitter:site",
+          name: "twitter:site",
+          content: "",
+        },
+      ],
+    },
   },
-  modules: ['@pinia/nuxt'],
+  buildModules: [],
+  modules: [
+    "@pinia/nuxt",
+    "nuxt-snackbar"
+  ],
+  snackbar: {
+    top: true,
+    right: true,
+    duration: 3000
+  },
   server: {
     port: process.env.PORT,
   },
   css: [
-    'vuetify/lib/styles/main.sass',
-    '@mdi/font/css/materialdesignicons.min.css',
+    "vuetify/lib/styles/main.sass",
+    "@mdi/font/css/materialdesignicons.min.css",
   ],
   build: {
-    transpile: ['vuetify'],
+    transpile: ["vuetify"],
   },
   vite: {
     define: {
-      'process.env.DEBUG': false,
+      "process.env.DEBUG": false,
     },
   },
-})
+  runtimeConfig: {
+    // The private keys which are only available within server-side
+    secret: "",
+    // Keys within public, will be also exposed to the client-side
+    public: {
+      baseUrl: process.env.BASE_URL,
+      apiUrl: process.env.API_URL,
+    },
+  },
+});
