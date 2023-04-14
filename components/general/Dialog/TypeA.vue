491925fcf411e1706db4c746759bd21619cfbf15
ahadi.mnazir <ahadi.mnazir@gmail.com>
2023-04-14 12:38:13 +0800

    update nav

--- components/general/Dialog/TypeA.vue
+++ components/general/Dialog/TypeA.vue
@@ -1,43 +1,47 @@
 <template lang="pug">
 v-dialog(
-    v-model="dialog"
-    width="100%"
-    max-width="600px"
-    scrollable
-    :persistent="persistent"
-    :fullscreen="$vuetify.display.width < 450 ? true : false"
-    :scrim="$vuetify.display.width < 450 ? false : true"
-    transition="dialog-bottom-transition"
-  )
-    v-card.rounded-lg(  )
-      v-card-title.d-flex.flex-row.align-center.justify-space-between.pa-5.font-weight-bold(style="font-size:18px")
-        slot(name="title")
-        v-icon.cursor-pointer.ml-auto(@click="$emit('update:modelValue', false)") mdi-close-circle-outline
-      v-card-text.pa-5
-        slot(name="content")
-      v-card-actions.pa-5
-        v-spacer
-        slot(name="action")
+  v-model="dialog",
+  width="100%",
+  max-width="600",
+  scrollable,
+  :persistent="persistent",
+  :fullscreen="$vuetify.display.width < 450 ? true : false",
+  :scrim="$vuetify.display.width < 450 ? false : true",
+  transition="dialog-bottom-transition"
+)
+  v-card.rounded-lg
+    v-card-title.d-flex.flex-row.align-center.justify-space-between.pa-5.font-weight-bold(
+      style="font-size: 18px"
+    )
+      slot(name="title")
+      v-icon.cursor-pointer.ml-auto(@click="$emit('update:modelValue', false)") mdi-close-circle-outline
+    v-card-text.pa-5
+      slot(name="content")
+    v-card-actions.pa-5
+      v-spacer
+      slot(name="action")
 </template>
 
 <script setup>
-const props = defineProps(
-  {
-    modelValue: Boolean,
-    persistent: {
-      type: Boolean,
-      default: true
-    }
-  }
-)
-const emit = defineEmits(['update:modelValue'])
+const props = defineProps({
+  modelValue: Boolean,
+  persistent: {
+    type: Boolean,
+    default: true,
+  },
+  maxWidth: {
+    type: String,
+    default: "600",
+  },
+});
+const emit = defineEmits(["update:modelValue"]);
 
 const dialog = computed({
   get() {
-    return props.modelValue
+    return props.modelValue;
   },
   set(value) {
-    emit('update:modelValue', value)
-  }
-})
+    emit("update:modelValue", value);
+  },
+});
 </script>
--- components/nav/MainNavDrawer.vue
+++ components/nav/MainNavDrawer.vue
@@ -1,113 +1,127 @@
 <template lang="pug">
 v-app
-  v-navigation-drawer(
-      v-model="drawer"
-    )
-
+  v-navigation-drawer(v-model="drawer")
     v-list.pa-0.d-flex.flex-column.h-100
-      v-list-group
-        template( v-slot:activator="{ props }" ) 
-          v-list-item.py-3(
-            v-bind="props"
-            prepend-avatar="https://ik.imagekit.io/hanifrodili/HNFRDL.jpg?updatedAt=1671006782759"
-            title="HNFRDL Store"
-            append-icon=""
-          )
-        v-list-item(@click="openSelectCompany = true" style="padding-left:20px !important")
-          template(v-slot:prepend)
-            v-icon.mr-2.icon-nav mdi-swap-horizontal
-          v-list-item-title.menu-text {{ $t('switchaccount') }}
+      .pa-4.d-flex.align-center
+        v-avatar(size="40")
+          v-img(
+            src="https://ik.imagekit.io/hanifrodili/HNFRDL.jpg?updatedAt=1671006782759")
+
+        .text-h6.ml-4
+          b HNFRDL Store
 
       v-divider
 
-      v-list-item.px-5(v-for="(nav, index) in navList" :key="index" @click="$router.push(nav.path)" :class="$route.name.split('-')[0] == nav.name ? 'activated' : ''")
+      v-list-item.px-5(
+        v-for="(nav, index) in navList",
+        :key="index",
+        @click="$router.push(nav.path)",
+        :class="$route.name.split('-')[0] == nav.name ? 'bg-secondary text-neutralLight' : ''")
         template(v-slot:prepend)
-          v-icon.mr-2.icon-nav {{ `mdi-${nav.mdi}` }}
-        div.d-flex.flex-row.align-center.justify-space-between
-          v-list-item-title.menu-text {{ $t(nav.i18n_key) }}
-          div.rounded-pill.bg-red.d-flex.align-center.justify-center(style="height:25px; padding:5px" v-if="nav.notification" )
-            p.mb-0(style="font-size:14px") {{ nav.notification }}
+          v-icon.mr-2(color="neutralLight") {{ `mdi-${nav.mdi}` }}
+        .d-flex.flex-row.align-center.justify-space-between
+          .text-body-2
+            b {{ $t(nav.i18n_key) }}
+          v-avatar(color="red", size="30", v-if="nav.notification")
+            b {{ nav.notification }}
 
       v-spacer
       v-divider
 
-      //- v-list-item.px-5(@click="openSelectLanguage = true")
-      //-   template(v-slot:prepend)
-      //-     v-icon.mr-2.icon-nav mdi-translate
-      //-   div.d-flex.flex-row.align-center.justify-space-between
-      //-     v-list-item-title.menu-text {{ $t('language') }}
+      .d-flex.flex-row.align-center.justify-center
+        v-btn(@click="openSelectLanguage = true", icon, flat)
+          .d-flex.flex-column.align-center.justify-center
+            v-icon(size="x-small") mdi-translate
+            .text-caption.mt-n1 {{ $t("language") }}
 
-      .d-flex.flex-row.align-center.justify-start.pa-4
-        v-btn.elevation-0.text-neutral( @click="openSelectLanguage = true" variant="text" rounded)
-          div.d-flex.flex-column.align-center.justify-center
-            v-icon mdi-translate
-            p(style="font-size:7px") {{ $t('language') }}
+        v-btn(
+          flat,
+          @click="toggleTheme",
+          :icon="theme.global.current.value.dark ? 'mdi-white-balance-sunny' : 'mdi-weather-night'",
+          :style="!theme.global.current.value.dark ? 'transform: rotate(0deg)' : 'transform: rotate(-90deg)'")
 
-        //- v-btn.elevation-0.text-neutral( @click="toggleTheme" variant="text" :icon="theme.global.current.value.dark ? 'mdi-white-balance-sunny' : 'mdi-weather-night'" :style="!theme.global.current.value.dark ? 'transform: rotate(0deg)' : 'transform: rotate(-90deg)'")
+  v-app-bar.elevation-1
+    v-app-bar-nav-icon(variant="text", @click.stop="drawer = !drawer")
+    v-toolbar-title.ml-0.ml-md-4.page-title {{ $t($route.name.split("-")[0]) }}
 
-  v-app-bar.elevation-1.bg-neutralDark()
-    v-app-bar-nav-icon(variant="text" @click.stop="drawer = !drawer")
-    v-toolbar-title.ml-0.ml-md-4.page-title {{ $t($route.name.split('-')[0]) }}
+    v-spacer
 
-    //- v-spacer
+    //- v-btn(
+      icon="mdi-help-circle-outline",
+      variant="icon",
+      width="35",
+      height="35")
 
-    v-btn.elevation-0.text-neutral.bg-transparent(size="small" icon="mdi-help-circle-outline" variant="text" width="28" height="28")
-
-    v-menu()
-      template( v-slot:activator="{ props }" )
-        v-btn.elevation-0.text-neutral.bg-transparent.mr-2(size="small" icon="mdi-bell-outline" variant="text" v-bind="props" width="28" height="28")
+    v-menu
+      template(v-slot:activator="{ props }")
+        v-btn(
+          icon="mdi-bell-outline",
+          variant="icon",
+          v-bind="props",
+          width="35",
+          height="35")
       v-list.pa-0
-        v-list-item()
+        v-list-item
           v-list-item-title Nothing new.
-    
-    v-menu()
-      template( v-slot:activator="{ props }" )
-        v-btn.elevation-0.text-neutral.bg-transparent(icon="mdi-account" size="small" variant="outlined" v-bind="props")
+
+    v-menu
+      template(v-slot:activator="{ props }")
+        v-btn(
+          icon="mdi-account-outline",
+          variant="icon",
+          width="35",
+          height="35",
+          v-bind="props")
       v-list.pa-0
         v-list-item(@click="openSelectCompany = true")
           template(v-slot:prepend)
             v-icon.mr-2.icon-nav mdi-swap-horizontal
-          v-list-item-title {{ $t('switchaccount') }}
-        v-list-item(@click="logout" v-if="userStore.user.email != ''")
-          v-list-item-title {{ $t('logout') }}
+          v-list-item-title {{ $t("switchaccount") }}
+        v-list-item(@click="logout", v-if="userStore.user.email != ''")
+          v-list-item-title {{ $t("logout") }}
 
-  v-main(style="min-height:100vh")
+  v-main(style="min-height: 100vh")
     slot
+
+  //- dialogs
   //- Language dialog
-  v-dialog( v-model="openSelectLanguage" width="100%" max-width="350px")
-    v-card(  )
-      v-card-title {{ $t('chooselanguage') }}
-      v-card-text
-        v-list.pa-0
-          v-list-item.rounded.mb-1(@click="setLanguage('en')" :class="$i18n.locale == 'en' ? 'bg-primary' : ''")
-            v-list-item-title English
-          v-list-item.rounded(@click="setLanguage('ms')" :class="$i18n.locale == 'ms' ? 'bg-primary' : ''")
-            v-list-item-title B. Melayu
+  general-dialog-type-a(v-model="openSelectLanguage", max-width="450")
+    template(#title) {{ $t("chooselanguage") }}
+    template(#content)
+      v-list
+        v-list-item.rounded.mb-1(
+          @click="setLanguage('en')",
+          :class="$i18n.locale == 'en' ? 'bg-primary' : ''")
+          v-list-item-title English
+        v-list-item.rounded(
+          @click="setLanguage('ms')",
+          :class="$i18n.locale == 'ms' ? 'bg-primary' : ''")
+          v-list-item-title B. Melayu
 
   //- Company dialog
-  v-dialog( v-model="openSelectCompany" width="100%" max-width="350px")
-    v-card(  )
-      v-card-title {{ $t('chooseaccount') }}
-      v-card-text
-        v-list.pa-0
-          v-list-item.rounded.mb-1(@click="" :class="true ? 'bg-primary' : ''")
-            v-list-item-title HNFRDL Store
-          v-list-item.rounded(@click="" :class="false ? 'bg-primary' : ''")
-            v-list-item-title BestFood Store
-
+  general-dialog-type-a(v-model="openSelectCompany", max-width="450")
+    template(#title) {{ $t("chooseaccount") }}
+    template(#content)
+      v-list
+        v-list-item.rounded-lg.mb-1(
+          @click="",
+          :class="true ? 'bg-primary' : ''")
+          v-list-item-title HNFRDL Store
+        v-list-item.rounded-lg(@click="", :class="false ? 'bg-primary' : ''")
+          v-list-item-title BestFood Store
 </template>
 
 <script setup>
-import { useStoreUser } from '~/store/storeMerchant'
-import { onMounted } from 'vue';
-import { useDisplay, useTheme } from 'vuetify'
-import { useI18n } from 'vue-i18n'
+import { useStoreUser } from "~/store/storeMerchant";
+import { onMounted } from "vue";
+import { useDisplay, useTheme } from "vuetify";
+import { useI18n } from "vue-i18n";
 
-const theme = useTheme()
-const display = useDisplay()
-const { locale } = useI18n({ useScope: 'global' })
-const userStore = useStoreUser()
-const router = useRouter()
+const theme = useTheme();
+const display = useDisplay();
+const { locale } = useI18n({ useScope: "global" });
+const userStore = useStoreUser();
+const router = useRouter();
 
 const navList = ref([
   {
@@ -115,120 +129,97 @@ const navList = ref([
     name: "dashboard",
     mdi: "view-dashboard",
     i18n_key: "dashboard",
-    notification: 0
+    notification: 0,
   },
   {
     path: "/order",
     name: "order",
     mdi: "inbox-arrow-down",
     i18n_key: "order",
-    notification: 20
+    notification: 20,
   },
   {
     path: "/customer",
     name: "customer",
     mdi: "account-group",
-    i18n_key:"customer",
-    notification: 30
+    i18n_key: "customer",
+    notification: 30,
   },
   {
     path: "/product",
     name: "product",
     mdi: "package",
     i18n_key: "product",
-    notification: 0
+    notification: 0,
   },
   {
     path: "/pages",
     name: "pages",
     mdi: "file-link",
     i18n_key: "pages",
-    notification: 0
+    notification: 0,
   },
   {
     path: "/settings",
     name: "settings",
     mdi: "cogs",
     i18n_key: "settings",
-    notification: 0
+    notification: 0,
   },
-])
+]);
 
-const drawer = ref(false)
-const openSelectLanguage = ref(false)
-const openSelectCompany = ref(false)
+const drawer = ref(false);
+const openSelectLanguage = ref(false);
+const openSelectCompany = ref(false);
 
 onMounted(() => {
-  locale.value = window.localStorage.getItem('preferredLanguage') || 'en'
-  theme.global.name.value = window.localStorage.getItem('preferredTheme') || 'light'
-  userStore.getUser()
-})
+  locale.value = window.localStorage.getItem("preferredLanguage") || "en";
+  theme.global.name.value =
+    window.localStorage.getItem("preferredTheme") || "light";
+  userStore.getUser();
+});
 
 watch(drawer, (newDrawerVal) => {
   if (display.width.value < 1280) {
     if (newDrawerVal) {
-      document.querySelector(".app").style.overflow = 'hidden'
-      document.querySelector(".app").style.height = '100vh'
+      document.querySelector(".app").style.overflow = "hidden";
+      document.querySelector(".app").style.height = "100vh";
     } else {
-      document.querySelector(".app").style.overflow = 'auto'
-      document.querySelector(".app").style.height = 'unset'
+      document.querySelector(".app").style.overflow = "auto";
+      document.querySelector(".app").style.height = "unset";
     }
   }
-})
+});
 
 function toggleTheme() {
-  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
-  window.localStorage.setItem('preferredTheme', theme.global.name.value)
+  theme.global.name.value = theme.global.current.value.dark ? "light" : "dark";
+  window.localStorage.setItem("preferredTheme", theme.global.name.value);
 }
 
 function setLanguage(lang) {
-  locale.value = lang
-  openSelectLanguage.value = false
-  window.localStorage.setItem('preferredLanguage', lang)
+  locale.value = lang;
+  openSelectLanguage.value = false;
+  window.localStorage.setItem("preferredLanguage", lang);
 }
 
 function logout() {
-  userStore.setUser()
-  router.push("/signin")
+  userStore.setUser();
+  router.push("/signin");
 }
 </script>
 
 <style lang="scss" scoped>
-.menu-text {
-  color: rgb(var(--v-theme-neutralLight));
-  padding: 10px 0px;
-  border-bottom: 2px solid transparent;
-  cursor: pointer;
-  font-size: 14px;
-  font-weight: bold;
-  vertical-align: middle;
-}
-
-.icon-nav{
-  color: rgb(var(--v-theme-neutralLight));
-}
-
 .v-list-item.activated {
   background-color: rgb(var(--v-theme-primary));
-  
-  .menu-text{
-    color: #000;
-  }
 
-  .v-icon{
-    color:#000
+  .v-icon {
+    color: #000;
   }
 }
 
 :deep(.v-navigation-drawer__scrim) {
-    background-color: rgba(0,0,0,.2);
-    opacity: 1;
-    backdrop-filter: blur(2px);
-}
-
-@media(max-width: 500px){
-  .menu-text {
-  padding: 0;
-}
+  background-color: rgba(0, 0, 0, 0.2);
+  opacity: 1;
+  backdrop-filter: blur(2px);
 }
 </style>
