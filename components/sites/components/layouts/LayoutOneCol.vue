0db350f492a1815ab5f36a4fea88c31bfab97047
Hanif Rodili <hanifrodili@gmail.com>
2023-06-14 14:28:40 +0800

    update

--- components/sites/components/layouts/LayoutOneCol.vue
+++ components/sites/components/layouts/LayoutOneCol.vue
@@ -3,8 +3,9 @@ v-row.ma-0(style="height:fit-content;" :style="`padding:${padding?.top}px ${padd
   v-col.d-flex.align-center.justify-center(v-if="components[0]" cols="12")
     component(:is="components[0].component" :data="components[0]" :editable="editMode" @input="updateContent(components[0]._uid)" data-placeholder="Your Text Here")
   v-col(v-if="!components[0] && editMode" cols="12")
-    div.d-flex.align-center.addNew.ignored()
+    div.d-flex.align-center.addNew.ignored(style="position:relative")
       sites-builder-add-block(:position="-1" @addBlock="addBlock" :blockList="Blocks")
+      v-icon.cursor-pointer.ml-auto.mr-4(@click="$emit('deleteBlock', data._uid)" size="x-small" color="red" style="position:absolute; bottom:10px; right:0") mdi-trash-can-outline
 
 </template>
 
@@ -26,7 +27,7 @@ const props = defineProps({
   }
 })
 
-const emits = defineEmits(['updateContent', 'addChild'])
+const emits = defineEmits(['updateContent', 'addChild', 'deleteBlock'])
 
 const components = ref(props.data.childBlock)
 
@@ -54,23 +55,46 @@ const addBlock = (pos, block) => {
   const name = block.replace(" ", "")
   const newBlockID = randID(10)
   const reuseBlockID = randNum(5)
+  let cssSetup = {}
+  if (name === 'Image') {
+    cssSetup = {
+      width: 600,
+      borderRadius: 10
+    }
+  } else if (name === 'Button') {
+    cssSetup = {
+      padding: {
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0
+      },
+      backgroundColor: "#1798bc",
+      font: {
+        family: 'Open Sans',
+        size: '500',
+      }
+    }
+  } else {
+    cssSetup = {
+      padding: {
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0
+      },
+      font: {
+        family: 'Open Sans',
+        size: '500',
+      }
+    }
+  }
   let newBlock = {
     _uid: newBlockID,
     component: name,
     name: name,
     config: {
-      css: {
-        padding: {
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0
-        },
-        font: {
-          family: 'Open Sans',
-          size: 'revert',
-        }
-      }
+      css: cssSetup
     }
   }
   components.value.splice(pos, 0, newBlock)
@@ -85,7 +109,7 @@ const updateContent = (id) => {
   }
 
   // console.log(content);
-  emits('updateContent', {parentId:props.data._uid, elementId:id, content:content})
+  emits('updateContent', { parentId: props.data._uid, elementId: id, content: content })
 }
 
 const randID = (len) => {
@@ -119,22 +143,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.addNew{
-  height:100px;
+.addNew {
+  height: 100px;
   border: 1px dashed rgb(var(--v-theme-secondary));
   opacity: .5;
-  position:relative;
+  position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
 }
 
-:deep(.addNew:hover){
+:deep(.addNew:hover) {
   border: 1px dashed rgb(var(--v-theme-primary));
   opacity: 1;
 
-  .addBlock{
-    .btn-add{
+  .addBlock {
+    .btn-add {
       color: rgb(var(--v-theme-primary));
       border-color: rgb(var(--v-theme-primary));
     }
@@ -142,29 +166,31 @@ export default {
 }
 
 
-.menuBtn{
+.menuBtn {
   width: 100%;
   opacity: .5;
 }
 
-.menuBtn:hover{
+.menuBtn:hover {
   opacity: 1;
 }
 
-:deep([contenteditable]):empty:after{
+:deep([contenteditable]):empty:after {
   content: attr(data-placeholder);
   color: rgb(212, 212, 212);
   // display: none;
 }
-:deep([contenteditable]):focus:empty:after{
+
+:deep([contenteditable]):focus:empty:after {
   content: attr(data-placeholder);
   color: rgb(212, 212, 212);
   // display: unset;
 }
+
 :deep([contenteditable]) {
-    -webkit-tap-highlight-color: transparent;
+  -webkit-tap-highlight-color: transparent;
 }
-:deep([contenteditable]):focus-visible{
+
+:deep([contenteditable]):focus-visible {
   outline: none;
-}
-</style>
+}</style>
